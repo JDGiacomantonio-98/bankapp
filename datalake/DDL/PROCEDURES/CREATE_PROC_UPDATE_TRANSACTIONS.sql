840730d6f8cf7d5e1de10cf95a6dad55a8412f35
J.DGiacomantonio <jacopodigiacomantonio@gmail.com>
2023-02-19 02:20:08 +0100

    parser and db manager ready to stream data to db

---
+++ datalake/DDL/PROCEDURES/CREATE_PROC_UPDATE_TRANSACTIONS.sql
@@ -0,0 +1,32 @@
+DROP PROCEDURE IF EXISTS update_transactions;
+CREATE PROCEDURE update_transactions()
+BEGIN
+	# this procedures scans everytime the transactions_dump table to gather only the last updated transaction values
+    # this procudere, while unefficient because it parses the whole dataset each time, accomodates the lack of MERGE statement in MySQL
+    
+	DELETE FROM transactions WHERE TRUE;
+    INSERT INTO transactions
+	SELECT 
+		FLOW_ID,
+		EXEC,
+		PROVIDER,
+		TR_DATE,
+		EXTRACT(YEAR FROM TR_DATE) TR_YEAR,
+		TR_WEEK,
+		TR_MONTH,
+		TR_DAY,
+		TR_WEEKDAY,
+		TR_HOUR,
+		TR_TYPE,
+		TR_LOCATION,
+		TR_BENEFICIARY,
+		TR_METHOD,
+		#TR_INFO,
+		TR_VALUE,
+		TR_UOM
+	FROM
+	(
+		SELECT *, ROW_NUMBER() OVER (PARTITION BY PROVIDER,TR_INFO ORDER BY EXEC DESC) RK from transactions_dump
+	) L0
+	WHERE RK = 1;
+END;
---
+++ main.py
@@ -0,0 +1,90 @@
+from mysql.connector import connect, Error as SQLError
+from os import path, mkdir, listdir, rename
+
+sql_db_instance = "bankapp"
+sql_db_env = "dev"
+app_uri = "C:\\Users\\Jacopo\\Desktop\\code-workbench\\"+sql_db_instance
+ddl_location= app_uri+"\\"+sql_db_env+"\\datalake\\DDL"
+dml_staging_location= app_uri+"\\"+sql_db_env+"\\datalake\\DML\\STAGING"
+dml_ingested_location= app_uri+"\\"+sql_db_env+"\\datalake\\DML\\INGESTED"
+
+for i in range(0, dml_ingested_location.split(app_uri)[1].count("\\")):
+	mkdir(path.join(app_uri,"\\".join(dml_ingested_location.split(app_uri+"\\")[1].split("\\")[0:i+1]))) if not(path.isdir(path.join(app_uri,"\\".join(dml_ingested_location.split(app_uri+"\\")[1].split("\\")[0:i+1])))) else next
+
+try:
+	conn = connect(
+		host="127.0.0.1",
+		user="root",
+		passwd=input("db login psw: "),
+		database=sql_db_instance
+		)
+except SQLError as e:
+	print("MySQL error: ", e)
+	if e.errno == 1049:
+		if input(f"Create {sql_db_instance} db from scratch? (y/n): ") in "yY":
+			try:
+				conn = connect(
+					host="127.0.0.1",
+					user="root",
+					passwd=input("insert psw: ")
+				)
+			except SQLError as e:
+				print("MySQL: ", e)
+				quit()
+			else:
+				cursor = conn.cursor()
+				cursor.execute(f"CREATE DATABASE {sql_db_instance};") # creates db
+				cursor.execute(f"USE {sql_db_instance}")
+
+				# creates all tables
+				for file in listdir(ddl_location+"\\TABLES"):
+					with open(ddl_location+"\\TABLES\\"+file,"r") as ddl:
+						cursor.execute(ddl.read())
+
+				# populates all tables for which dml exists
+				for file in listdir(dml_ingested_location):
+					with open(dml_ingested_location+"\\"+file,"r") as dml:
+						for row in dml.readlines()[:-1]:
+							cursor.execute(row)
+						conn.commit()
+							
+				for file in listdir(dml_staging_location):
+					with open(dml_staging_location+"\\"+file,"r") as dml:
+						for row in dml.readlines()[:-1]:
+							cursor.execute(row)
+						conn.commit()
+
+				# creates all procedures
+				for file in listdir(ddl_location+"\\PROCEDURES"):
+					with open(ddl_location+"\\PROCEDURES\\"+file,"r") as ddl:
+						cursor.execute(ddl.read())
+
+				conn.close()
+				
+				print("database created!")
+
+				conn = connect(
+					host="127.0.0.1",
+					user="root",
+					passwd=input("insert psw to populate db: "),
+					database=sql_db_instance
+				)
+				cursor=conn.cursor()
+				print("running ..")
+				cursor.callproc("update_transactions")
+				conn.commit()
+				print("db is up to date!")
+		else:
+			quit()
+else:
+	cursor = conn.cursor()
+	for filename in listdir(dml_staging_location):
+		with open(dml_staging_location+"\\"+filename,"r") as dml:
+			for row in dml.readlines()[:-1]:
+				cursor.execute(row)
+		conn.commit()
+		rename(src=dml.name,dst=path.join(dml_ingested_location,filename))
+
+	cursor.callproc("update_transactions")
+	conn.commit()
+	print("db is up to date!")
