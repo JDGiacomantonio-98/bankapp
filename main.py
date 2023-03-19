1e448d356488159d01ab1e376cce2c830851db36
J.DGiacomantonio <jacopodigiacomantonio@gmail.com>
2023-03-19 20:00:31 +0100

    modular main and workers. Added waiting logic when requesting new transaction report

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from mysql.connector import connect, Error as SQLError
 from os import getenv,path, mkdir, listdir, rename
+from time import sleep, process_time
+import webbrowser
+import worker
 
 sql_db_instance = "bankapp"
 sql_db_env = "dev"
@@ -10,113 +13,161 @@ ddl_location= path.join(app_uri,sql_db_env,'datalake','DDL')
 dml_staging_location= path.join(app_uri,sql_db_env,'datalake','DML','STAGING')
 dml_ingested_location= path.join(app_uri,sql_db_env,'datalake','DML','INGESTED')
 
-for i in range(0, dml_ingested_location.split(app_uri)[1].count("\\")):
-	mkdir(path.join(app_uri,"\\".join(dml_ingested_location.split(app_uri+"\\")[1].split("\\")[0:i+1]))) if not(path.isdir(path.join(app_uri,"\\".join(dml_ingested_location.split(app_uri+"\\")[1].split("\\")[0:i+1])))) else next
+mps_provider_url = "https://digital.mps.it/pri/login/home_mobile.jsp?_ga=2.215198551.1509105742.1620041788-785024715.1610638296"
 
-try:
-	conn = connect(
-		host="127.0.0.1",
-		user="root",
-		passwd=input("db login psw: ")
-		)
-	cursor = conn.cursor()
+def create_db_if_not_exists(conn, cursor):
+	print(f"creating '{sql_db_instance}' db from scratch ..")
+
+	cursor.execute(f"CREATE DATABASE {sql_db_instance};") # creates db
 	cursor.execute(f"USE {sql_db_instance};")
-except SQLError as e:
-	print("MySQL error: ", e)
-	if e.errno == 1049:
-		if input(f"Create {sql_db_instance} db from scratch? (y/n): ") in "yY":
-			try:
-				cursor = conn.cursor()
-				cursor.execute(f"CREATE DATABASE {sql_db_instance};") # creates db
-				cursor.execute(f"USE {sql_db_instance};")
 
-				# creates all tables
-				for file in listdir(ddl_location+"\\TABLES"):
-					if file == ".gitkeep":
-						continue
+	# creates all tables
+	i = 0
+	for file in listdir(ddl_location+"\\TABLES"):
+		if file == ".gitkeep":
+			continue
 
-					with open(ddl_location+"\\TABLES\\"+file,"r") as ddl:
-						cursor.execute(ddl.read())
+		with open(ddl_location+"\\TABLES\\"+file,"r") as ddl:
+			cursor.execute(ddl.read())
+			i+=1
+	print(f"{i} tables created!")
 
-				# populates all tables for which dml exists
-				for file in listdir(dml_ingested_location):
-					if file == ".gitkeep":
-						continue
+	# populates all tables for which dml exists
+	print("populating tables ..")
+	ingest_data(conn=conn, cursor=cursor, dml_location=dml_ingested_location, deploy_worker=False)
+	ingest_data(conn=conn, cursor=cursor, dml_location=dml_staging_location, deploy_worker=False)
 
-					with open(dml_ingested_location+"\\"+file,"r") as dml:
-						for row in dml.readlines()[:-1]:
-							cursor.execute(row)
-						conn.commit()
+	# creates all views
+	i = 0
+	for file in listdir(ddl_location+"\\VIEWS"):
+		if file == ".gitkeep":
+			continue
 
-				# populates all tables for which dml exists
-				for file in listdir(dml_staging_location):
-					if file == ".gitkeep":
-						continue
+		with open(ddl_location+"\\VIEWS\\"+file,"r") as ddl:
+			cursor.execute(ddl.read())
+			i+=1
+	print(f"{i} views created!")
 
-					with open(dml_staging_location+"\\"+file,"r") as dml:
-						for row in dml.readlines()[:-1]:
-							cursor.execute(row)
-						conn.commit()
+	# creates all procedures
+	i=0
+	ddl = ""
+	for file in listdir(ddl_location+"\\PROCEDURES"):
+		if file == ".gitkeep":
+			continue
+	
+		with open(ddl_location+"\\PROCEDURES\\"+file,"r") as f:
+			ddl += "\n" + f.read()
+			i+=1
+	cursor.execute(ddl)
+	print(f"{i} procedures created!")
 
-				# creates all views
-				for file in listdir(ddl_location+"\\VIEWS"):
-					if file == ".gitkeep":
-						continue
+	print(f"database '{sql_db_instance}' successfully created!")
 
-					with open(ddl_location+"\\VIEWS\\"+file,"r") as ddl:
-						cursor.execute(ddl.read())
+def ingest_data(conn, cursor, dml_location=dml_staging_location, deploy_worker=True):
 
-				# creates all procedures
-				ddl = ""
-				for file in listdir(ddl_location+"\\PROCEDURES"):
-					if file == ".gitkeep":
-						continue
-				
-					with open(ddl_location+"\\PROCEDURES\\"+file,"r") as f:
-						ddl += "\n" + f.read()
-				cursor.execute(ddl)
-				
-				conn.close()
-				
-				print("database created!")
-				
-			except SQLError as e:
-				print("MySQL: ", e)
-				quit()
-			else:
-				conn = connect(
-					host="127.0.0.1",
-					user="root",
-					passwd=input("insert psw to populate db: "),
-					database=sql_db_instance
-				)
-				cursor=conn.cursor()
-				print("populating ..")
-				cursor.callproc("update_transactions")
-				cursor.callproc("update_calendar")
-				cursor.callproc("update_earnings")
-				print("..")
-				cursor.callproc("update_revenues")
-				conn.commit()
-				print("db is up to date!")
-		else:
-			quit()
-else:
-	for filename in listdir(dml_staging_location):
+	if deploy_worker:
+		try:
+			worker.parse_mps_file()
+		except FileNotFoundError:
+			print()
+			print("No new transaction report found in the Download folder.")
+			if input("Do you want to extract a new transactions report from MPS website? (y/n): ") in "yY":
+				webbrowser.open_new(mps_provider_url)
+				print("waiting for new file to be detected in Download folder ..")
+				sleep(80) # gives user time to login on MPS home banking and extract the report, while offloading process resources
+				i = 1
+				while True:
+					try:
+						worker.parse_mps_file()
+					except FileNotFoundError:
+						if i == 1:
+							sleep(40)
+							t = process_time()
+							continue
+
+						if process_time()-t > 59*i:
+							if input("Python is waiting for a new file to land on the Dowload folder, do you want to stop it? (y/n) ") in "yY":
+								break
+							else:
+								i+=1 #wait for an additional minute before asking again
+					except Exception as e:
+						print(f"Unknown problem while parsing the report: {e}")
+						print("db update failed.")
+						quit()
+					else:
+						break
+
+	for filename in listdir(dml_location):
 		if filename == ".gitkeep":
 			continue
 
-		with open(dml_staging_location+"\\"+filename,"r") as dml:
+		with open(dml_location+"\\"+filename,"r") as dml:
 			for row in dml.readlines()[:-1]:
 				cursor.execute(row)
-		conn.commit()
-		rename(src=dml.name,dst=path.join(dml_ingested_location,filename))
+			conn.commit()
 
-	print("updating db ..")
-	cursor.callproc("update_transactions")
-	cursor.callproc("update_calendar")
-	cursor.callproc("update_earnings")
-	print("..")
-	cursor.callproc("update_revenues")
-	conn.commit()
-	print("db is up to date!")
+		if dml_location!=dml_ingested_location:
+			rename(src=dml.name,dst=path.join(dml_ingested_location,filename))
+
+def elevate_data(conn, cursor):
+	try:
+		print("updating transactions ..")
+		cursor.callproc("update_transactions")
+		
+		print("updating calendar ..")
+		cursor.callproc("update_calendar")
+
+		print("updating earnings ..")
+		cursor.callproc("update_earnings")
+
+		print("updating revenues ..")
+		cursor.callproc("update_revenues")
+	except Exception as e:
+		print(f"MySQL error:{e}")
+		quit()
+	else:
+		conn.commit()
+		print(f"db '{sql_db_instance}' is up to date!")
+
+def update_db():
+	try:
+		conn = connect(
+			host="127.0.0.1",
+			user="root",
+			passwd=input("db login psw: ")
+			)
+		cursor = conn.cursor()
+		cursor.execute(f"USE {sql_db_instance};")
+	except SQLError as e:
+		print("MySQL error: ", e)
+		if e.errno == 1049:
+			print(f"No instance of {sql_db_instance} db found.")
+			if input(f"Create {sql_db_instance} db from scratch? (y/n): ") in "yY":
+				try:
+					create_db_if_not_exists(conn=conn, cursor=cursor)
+					conn.close()
+
+				except SQLError as e:
+					print("MySQL: ", e)
+					quit()
+				else:
+					conn = connect(
+						host="127.0.0.1",
+						user="root",
+						passwd=input("insert psw to elevate data: "),
+						database=sql_db_instance
+					)
+					elevate_data(conn=conn, cursor=conn.cursor())
+			else:
+				print("closing connection..")
+				quit()
+	else:
+		ingest_data(conn=conn, cursor=cursor)
+		elevate_data(conn=conn, cursor=cursor)
+
+if __name__ == "__main__":
+	
+	for i in range(0, dml_ingested_location.split(app_uri)[1].count("\\")):
+		mkdir(path.join(app_uri,"\\".join(dml_ingested_location.split(app_uri+"\\")[1].split("\\")[0:i+1]))) if not(path.isdir(path.join(app_uri,"\\".join(dml_ingested_location.split(app_uri+"\\")[1].split("\\")[0:i+1])))) else next
+
+	update_db()
--- worker.py
+++ worker.py
@@ -1,9 +1,8 @@
 import openpyxl as pyxl
-from time import time
+from time import time,sleep
 from os import path, getenv, mkdir, rename
 from random import uniform
 from io import BytesIO
-import webbrowser
 
 sql_db_instance = "bankapp"
 sql_db_env = "dev"
@@ -19,94 +18,103 @@ csv_dump_uri = path.join(app_uri,sql_db_env,'dump','provider','csv')
 
 revenues_owner = "SELF"
 
-for i in range(0, sql_dump_uri.split(app_uri)[1].count("\\")):
-	mkdir(path.join(app_uri,"\\".join(sql_dump_uri.split(app_uri+"\\")[1].split("\\")[0:i+1]))) if not(path.isdir(path.join(app_uri,"\\".join(sql_dump_uri.split(app_uri+"\\")[1].split("\\")[0:i+1])))) else next
+def create_bins():
+	for i in range(0, sql_dump_uri.split(app_uri)[1].count("\\")):
+		mkdir(path.join(app_uri,"\\".join(sql_dump_uri.split(app_uri+"\\")[1].split("\\")[0:i+1]))) if not(path.isdir(path.join(app_uri,"\\".join(sql_dump_uri.split(app_uri+"\\")[1].split("\\")[0:i+1])))) else next
 
-for i in range(0, raw_dump_uri.split(app_uri)[1].count("\\")):
-	mkdir(path.join(app_uri,"\\".join(raw_dump_uri.split(app_uri+"\\")[1].split("\\")[0:i+1]))) if not(path.isdir(path.join(app_uri,"\\".join(raw_dump_uri.split(app_uri+"\\")[1].split("\\")[0:i+1])))) else next
-mkdir(csv_dump_uri) if not(path.isdir(csv_dump_uri)) else next
+	for i in range(0, raw_dump_uri.split(app_uri)[1].count("\\")):
+		mkdir(path.join(app_uri,"\\".join(raw_dump_uri.split(app_uri+"\\")[1].split("\\")[0:i+1]))) if not(path.isdir(path.join(app_uri,"\\".join(raw_dump_uri.split(app_uri+"\\")[1].split("\\")[0:i+1])))) else next
+	mkdir(csv_dump_uri) if not(path.isdir(csv_dump_uri)) else next
 
-landed_file_uri  = path.join(getenv('USERPROFILE'),'Downloads','I miei movimenti conto.xlsx')
+def parse_mps_file():
+	landed_file_uri  = path.join(getenv('USERPROFILE'),'Downloads','I miei movimenti conto.xlsx')
 
-try:
-	with open(landed_file_uri, "rb") as f:
-		wb = pyxl.load_workbook(BytesIO(f.read()), data_only=True)
-except FileNotFoundError:
-	print()
-	print("No new transaction file available in the Download folder. Please download it from MPS home banking website.")
-	webbrowser.open_new("https://digital.mps.it/pri/login/home_mobile.jsp?_ga=2.215198551.1509105742.1620041788-785024715.1610638296")
-	quit()
-
-data = wb[wb.sheetnames[0]]
-
-exec = int(time())
-
-processed_filename = str(exec)+"_"+landed_file_uri.split("\\")[-1].replace(" ","_")
-
-sql_insert_name = f"INSERT_{sql_tablename}_{exec}.sql"
-sql_insert_dml = "" # empty insert sql statement into target tablename
-
-csv_name = f"TRANSACTIONS_{exec}.csv"
-csv = header + "\n" # initialise empty csv with specified header
-
-for r in data.iter_rows(min_col=3, max_col=7, min_row=20, values_only=True):
-	
-	r = ["TRANSACTIONS", exec, provider] + list(r) + ["EUR"] # adds metadata to  payload making it unique even if extraction is repeated w/ same parameters
-	
-	if "Totale" in r[5]:
-		wb.close()
-		break
-
-	r[5] = r[5].replace("\n"," ") # removes \n inserted by the provider when transaction description gets verbose
-	r.pop(6) # remove empty cell for each row, provider-specific file structure
-
-	r.insert(4,int(r[3].strftime("%W"))) # weeknum
-	r.insert(5,int(r[3].strftime("%m"))) # monthnum
-	r.insert(6,int(r[3].strftime("%d"))) # daynum
-	r.insert(7,int(r[3].strftime("%w"))) # weekday
-	r[3]=r[3].strftime("%Y-%m-%d")
-
-	if "Prelievo " in r[9]:
-		r.insert(8,int(r[9].split("Prelievo Self Service Atm ")[1].split(" ")[2].split(":")[0])) # hour
-	elif "Bon." in r[9] or  "Storno" in r[9] or "bollo" in r[8] or "Addebito" in r[8]:
-		r.insert(8,int(uniform(9, 20))) # hour randomly assigned due to lack of data in provider payload
-	else:
+	try:
+		with open(landed_file_uri, "rb") as f:
+			wb = pyxl.load_workbook(BytesIO(f.read()), data_only=True)
+	except FileNotFoundError:
+		raise FileNotFoundError
+	except Exception:
+		sleep(5) # give time to complete dowloading the file in case of concurrent file dowload completion and pyxl load_workbook
 		try:
-			r.insert(8,int(r[9].split(" Ora ")[1][0:2])) # hour
-		except IndexError:
-			r.insert(8,None) # hour
-
-	r.insert(9, "expense" if r[-2]<=0 else "revenue") # type
-
-	if "Prelievo " in r[11]:
-		r.insert(10," ".join(r[11].split("Prelievo Self Service ")[1].split(" ")[0:2])) # location
-	elif "Bon." in r[11]:
-		r.insert(10,"Home Banking") # location
+			with open(landed_file_uri, "rb") as f:
+				wb = pyxl.load_workbook(BytesIO(f.read()), data_only=True)
+		except Exception as e:
+			raise e # raise unknown exception and halt the caller process
+		
 	else:
-		try:
-			r.insert(10,r[11].split(" Loc.")[1].split(" Esercente" if " Esercente" in r[11] else " Imp.")[0].title()) # location
-		except IndexError:
-			r.insert(10,None) # location
+		print("1 new transactions file found in Download folder..")
+		data = wb[wb.sheetnames[0]]
 
-	if r[-2]>0:
-		r.insert(11,revenues_owner) # beneficiary
-	elif "Prel." in r[11]:
-		r.insert(11,"Unknown") # beneficiary
-	elif "Bon." in r[11] or "Addebito" in r[11]:
-		r.insert(11,r[12].split(" A Favore ")[1].split(" Iban" if " Iban" in r[12] else "Codice")[0].strip(" ")) # beneficiary
-	else:
-		try:
-			r.insert(11,r[12].split("Esercente")[1].split(" Imp.")[0].strip().lstrip(":").lstrip().title()) # beneficiary
-		except IndexError:
-			r.insert(11,None) # beneficiary
+		exec = int(time())
 
-	csv += str(r)[1:-1].replace(" '",' "').replace("',",'",') + "\n"
-	sql_insert_dml += f"INSERT INTO {sql_db_instance}.{sql_tablename} ({header}) VALUES ({str(r)[1:-1]});" + "\n"
+		processed_filename = str(exec)+"_"+landed_file_uri.split("\\")[-1].replace(" ","_")
 
-with open(csv_dump_uri+"\\"+csv_name, "w") as f:
-	f.write(csv.replace("\n'",'\n"').replace("'\n",'"\n'))
+		sql_insert_name = f"INSERT_{sql_tablename}_{exec}.sql"
+		sql_insert_dml = "" # empty insert sql statement into target tablename
 
-with open(sql_dump_uri+"\\"+sql_insert_name, "w") as f:
-	f.write(sql_insert_dml.replace("None","NULL"))
+		csv_name = f"TRANSACTIONS_{exec}.csv"
+		csv = header + "\n" # initialise empty csv with specified header
 
-rename(src=landed_file_uri,dst=path.join(raw_dump_uri,processed_filename))
+		print("parsing new report ..")
+		for r in data.iter_rows(min_col=3, max_col=7, min_row=20, values_only=True):
+			
+			r = ["TRANSACTIONS", exec, provider] + list(r) + ["EUR"] # adds metadata to  payload making it unique even if extraction is repeated w/ same parameters
+			
+			if "Totale" in r[5]:
+				wb.close()
+				break
+
+			r[5] = r[5].replace("\n"," ") # removes \n inserted by the provider when transaction description gets verbose
+			r.pop(6) # remove empty cell for each row, provider-specific file structure
+
+			r.insert(4,int(r[3].strftime("%W"))) # weeknum
+			r.insert(5,int(r[3].strftime("%m"))) # monthnum
+			r.insert(6,int(r[3].strftime("%d"))) # daynum
+			r.insert(7,int(r[3].strftime("%w"))) # weekday
+			r[3]=r[3].strftime("%Y-%m-%d")
+
+			if "Prelievo " in r[9]:
+				r.insert(8,int(r[9].split("Prelievo Self Service Atm ")[1].split(" ")[2].split(":")[0])) # hour
+			elif "Bon." in r[9] or  "Storno" in r[9] or "bollo" in r[8] or "Addebito" in r[8]:
+				r.insert(8,int(uniform(9, 20))) # hour randomly assigned due to lack of data in provider payload
+			else:
+				try:
+					r.insert(8,int(r[9].split(" Ora ")[1][0:2])) # hour
+				except IndexError:
+					r.insert(8,None) # hour
+
+			r.insert(9, "expense" if r[-2]<=0 else "revenue") # type
+
+			if "Prelievo " in r[11]:
+				r.insert(10," ".join(r[11].split("Prelievo Self Service ")[1].split(" ")[0:2])) # location
+			elif "Bon." in r[11]:
+				r.insert(10,"Home Banking") # location
+			else:
+				try:
+					r.insert(10,r[11].split(" Loc.")[1].split(" Esercente" if " Esercente" in r[11] else " Imp.")[0].title()) # location
+				except IndexError:
+					r.insert(10,None) # location
+
+			if r[-2]>0:
+				r.insert(11,revenues_owner) # beneficiary
+			elif "Prel." in r[11]:
+				r.insert(11,"Unknown") # beneficiary
+			elif "Bon." in r[11] or "Addebito" in r[11]:
+				r.insert(11,r[12].split(" A Favore ")[1].split(" Iban" if " Iban" in r[12] else "Codice")[0].strip(" ")) # beneficiary
+			else:
+				try:
+					r.insert(11,r[12].split("Esercente")[1].split(" Imp.")[0].strip().lstrip(":").lstrip().title()) # beneficiary
+				except IndexError:
+					r.insert(11,None) # beneficiary
+
+			csv += str(r)[1:-1].replace(" '",' "').replace("',",'",') + "\n"
+			sql_insert_dml += f"INSERT INTO {sql_db_instance}.{sql_tablename} ({header}) VALUES ({str(r)[1:-1]});" + "\n"
+
+		with open(csv_dump_uri+"\\"+csv_name, "w") as f:
+			f.write(csv.replace("\n'",'\n"').replace("'\n",'"\n'))
+
+		with open(sql_dump_uri+"\\"+sql_insert_name, "w") as f:
+			f.write(sql_insert_dml.replace("None","NULL"))
+
+		rename(src=landed_file_uri,dst=path.join(raw_dump_uri,processed_filename))
